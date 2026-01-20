10b235c91d362caffa3644eea2c41f0bee960d8a
Gabriel James Benedict <benok.loslos@gmail.com>
2026-01-20 18:01:37 +0800

    Initial Commit

---
+++ src/main/java/com/porado/worduel_backend/WorduelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.porado.worduel_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorduelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorduelBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porado/worduel_backend/WorduelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porado.worduel_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorduelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
