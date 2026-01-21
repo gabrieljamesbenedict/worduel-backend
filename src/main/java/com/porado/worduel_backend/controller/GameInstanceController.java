f47a5b281b138e6a35f645f58614ce44ded81e56
Gabriel James Benedict <benok.loslos@gmail.com>
2026-01-21 19:24:53 +0800

    feat: add controller classes for GameInstance, GameRoom, and GameStatus

---
+++ src/main/java/com/porado/worduel_backend/controller/GameInstanceController.java
@@ -0,0 +1,52 @@
+package com.porado.worduel_backend.controller;
+
+import com.porado.worduel_backend.model.GameInstance;
+import com.porado.worduel_backend.service.GameInstanceService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/games")
+@RequiredArgsConstructor
+public class GameInstanceController {
+
+    private final GameInstanceService gameInstanceService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GameInstance> getGameInstanceById(@PathVariable Long id) {
+        Optional<GameInstance> gameInstanceOptional = gameInstanceService.getGameInstanceById(id);
+
+        return gameInstanceOptional
+                .map(
+                        ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build()
+                );
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GameInstance>> getAllGameInstances() {
+        return ResponseEntity.ok(gameInstanceService.getAllGameInstances());
+    }
+
+    @PostMapping
+    public ResponseEntity<GameInstance> createGameInstance(@Valid @RequestBody GameInstance gameInstance) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(gameInstanceService.createGameInstance(gameInstance));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<GameInstance> updateGameInstance(@PathVariable Long id, @Valid @RequestBody GameInstance gameInstance) {
+        return ResponseEntity.ok(gameInstanceService.updateGameInstance(id ,gameInstance));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<GameInstance> deleteGameInstance(@PathVariable Long id) {
+        gameInstanceService.deleteGameInstanceById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ src/main/java/com/porado/worduel_backend/controller/GameRoomController.java
@@ -0,0 +1,53 @@
+package com.porado.worduel_backend.controller;
+
+import com.porado.worduel_backend.model.GameRoom;
+import com.porado.worduel_backend.service.GameRoomService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("rooms")
+@RequiredArgsConstructor
+public class GameRoomController {
+
+    private final GameRoomService gameRoomService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GameRoom> getGameRoomById(@PathVariable Long id) {
+        Optional<GameRoom> gameRoomOptional = gameRoomService.getGameRoomById(id);
+
+        return gameRoomOptional
+                .map(
+                        ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build()
+                );
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GameRoom>> getAllGameRooms() {
+        return ResponseEntity.ok(gameRoomService.getAllGameRooms());
+    }
+
+    @PostMapping
+    public ResponseEntity<GameRoom> createGameRoom(@Valid @RequestBody GameRoom gameRoom) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(gameRoomService.createGameRoom(gameRoom));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<GameRoom> updateGameRoom(@PathVariable Long id, @Valid @RequestBody GameRoom gameRoom) {
+        return ResponseEntity.ok(gameRoomService.updateGameRoom(id, gameRoom));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<GameRoom> deleteGameRoom(@PathVariable Long id) {
+        gameRoomService.deleteGameRoomById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
