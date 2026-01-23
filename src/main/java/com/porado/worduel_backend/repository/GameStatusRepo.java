2bfb7716a6327e700d9c654c2fa4b514af8a2885
Gabriel James Benedict <benok.loslos@gmail.com>
2026-01-23 14:43:24 +0800

    refactor: remove GameStatus controller, service, and repository classes temporarily

--- src/main/java/com/porado/worduel_backend/repository/GameStatusRepo.java
+++ src/main/java/com/porado/worduel_backend/repository/GameStatusRepo.java
@@ -1,7 +1,7 @@
-package com.porado.worduel_backend.repository;
-
-import com.porado.worduel_backend.model.GameStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameStatusRepo extends JpaRepository<GameStatus, Long> {
-}
+//package com.porado.worduel_backend.repository;
+//
+//import com.porado.worduel_backend.model.GameStatus;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface GameStatusRepo extends JpaRepository<GameStatus, Long> {
+//}
--- src/main/java/com/porado/worduel_backend/serviceImpl/GameStatusServiceImpl.java
+++ src/main/java/com/porado/worduel_backend/serviceImpl/GameStatusServiceImpl.java
@@ -1,45 +1,45 @@
-package com.porado.worduel_backend.serviceImpl;
-
-import com.porado.worduel_backend.model.GameStatus;
-import com.porado.worduel_backend.repository.GameStatusRepo;
-import com.porado.worduel_backend.service.GameStatusService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-@RequiredArgsConstructor
-public class GameStatusServiceImpl implements GameStatusService {
-
-    private final GameStatusRepo gameStatusRepo;
-
-    @Override
-    public Optional<GameStatus> getGameStatusById(Long id) {
-        return gameStatusRepo.findById(id);
-    }
-
-    @Override
-    public List<GameStatus> getAllGameStatuses() {
-        return gameStatusRepo.findAll();
-    }
-
-    @Override
-    public GameStatus createGameStatus(GameStatus gameStatus) {
-        return gameStatusRepo.save(gameStatus);
-    }
-
-    @Override
-    public GameStatus updateGameStatus(Long id, GameStatus gameStatus) {
-        if (!gameStatusRepo.existsById(id)) {
-            throw new RuntimeException("GameStatus not found with id " + id);
-        }
-        return gameStatusRepo.save(gameStatus);
-    }
-
-    @Override
-    public void deleteGameStatusById(Long id) {
-        gameStatusRepo.deleteById(id);
-    }
-}
+//package com.porado.worduel_backend.serviceImpl;
+//
+//import com.porado.worduel_backend.model.GameStatus;
+//import com.porado.worduel_backend.repository.GameStatusRepo;
+//import com.porado.worduel_backend.service.GameStatusService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class GameStatusServiceImpl implements GameStatusService {
+//
+//    private final GameStatusRepo gameStatusRepo;
+//
+//    @Override
+//    public Optional<GameStatus> getGameStatusById(Long id) {
+//        return gameStatusRepo.findById(id);
+//    }
+//
+//    @Override
+//    public List<GameStatus> getAllGameStatuses() {
+//        return gameStatusRepo.findAll();
+//    }
+//
+//    @Override
+//    public GameStatus createGameStatus(GameStatus gameStatus) {
+//        return gameStatusRepo.save(gameStatus);
+//    }
+//
+//    @Override
+//    public GameStatus updateGameStatus(Long id, GameStatus gameStatus) {
+//        if (!gameStatusRepo.existsById(id)) {
+//            throw new RuntimeException("GameStatus not found with id " + id);
+//        }
+//        return gameStatusRepo.save(gameStatus);
+//    }
+//
+//    @Override
+//    public void deleteGameStatusById(Long id) {
+//        gameStatusRepo.deleteById(id);
+//    }
+//}
