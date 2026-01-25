4fa524a7b5f6fd31398c94e1ebbca6323c7c2d9d
Gabriel James Benedict <benok.loslos@gmail.com>
2026-01-26 01:39:14 +0800

    feat: add password encoding via BCrypt

--- src/main/java/com/porado/backend/security/SecurityConfig.java
+++ src/main/java/com/porado/backend/security/SecurityConfig.java
@@ -1,28 +1,40 @@
 package com.porado.backend.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated())
+                        .anyRequest().permitAll()) // temporarily allow all for testing
                 .httpBasic(Customizer.withDefaults()) // remove this in production
         ;
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
     }
 
 }
--- src/main/java/com/porado/backend/security/controller/AuthController.java
+++ src/main/java/com/porado/backend/security/controller/AuthController.java
@@ -2,10 +2,12 @@ package com.porado.backend.security.controller;
 
 import com.porado.backend.model.Player;
 import com.porado.backend.repository.PlayerRepository;
+import com.porado.backend.repository.PlayerRoleRepository;
 import com.porado.backend.security.model.RegistrationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +21,8 @@ import java.net.http.HttpResponse;
 public class AuthController {
 
     private final PlayerRepository playerRepository;
+    private final PlayerRoleRepository playerRoleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
     private ResponseEntity<?> registerPlayer(@RequestBody RegistrationRequest registrationRequest) {
@@ -27,8 +31,9 @@ public class AuthController {
         }
 
         Player newPlayer = Player.builder()
-                .username("")
-                .password("")
+                .username(registrationRequest.getUsername())
+                .password(passwordEncoder.encode(registrationRequest.getPassword()))
+                .role(playerRoleRepository.findById(1L).orElseThrow(() -> new IllegalStateException("Role not found")))
                 .enabled(true)
                 .build();
         playerRepository.save(newPlayer);
