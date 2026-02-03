05d81ab10fbcad6012040c8fac0c2e70fe2eef53
Gabriel James Benedict <benok.loslos@gmail.com>
2026-02-04 03:01:38 +0800

    feat: add /me endpoint for checking auth status in the frontend.

--- src/main/java/com/porado/backend/security/controller/AuthController.java
+++ src/main/java/com/porado/backend/security/controller/AuthController.java
@@ -3,17 +3,21 @@ package com.porado.backend.security.controller;
 import com.porado.backend.model.Player;
 import com.porado.backend.repository.PlayerRepository;
 import com.porado.backend.repository.PlayerRoleRepository;
+import com.porado.backend.security.model.CustomUserDetails;
 import com.porado.backend.security.model.RegistrationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.net.http.HttpResponse;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/auth")
@@ -39,6 +43,21 @@ public class AuthController {
         playerRepository.save(newPlayer);
 
         return ResponseEntity.ok("User registered successfully");
+    }
+
+    @GetMapping("/me")
+    private ResponseEntity<?> me(Authentication authentication) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(401).body("Not authorized");
+        }
+
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        assert customUserDetails != null;
+        return ResponseEntity.status(200).body(Map.of(
+                "username", customUserDetails.getUsername(),
+                "roles", customUserDetails.getAuthorities()
+        ));
     }
 
 }
