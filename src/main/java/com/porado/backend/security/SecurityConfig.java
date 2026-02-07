f23794d17dbcad578d11242309aefb0cc31ef779
Gabriel James Benedict <benok.loslos@gmail.com>
2026-02-08 03:19:18 +0800

    feat: add a SecurityFilterChain for mobile apps

---
+++ src/main/java/com/porado/backend/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.porado.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain appSecurity(HttpSecurity httpSecurity) {
+        httpSecurity
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).authorizeHttpRequests(auth -> auth
+                        //.requestMatchers("/auth/**").permitAll()
+                        .anyRequest().permitAll() // Replace with .fullyAuthenticated() for prod
+                );
+
+        return httpSecurity.build();
+    }
+
+}
