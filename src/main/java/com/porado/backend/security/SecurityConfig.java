0e4443aae5b821b9be5c75a0d3a9371407e9a6bb
Gabriel James Benedict <benok.loslos@gmail.com>
2026-02-09 13:06:31 +0800

    refactor: create Basic Auth in security fitler chain

--- src/main/java/com/porado/backend/security/SecurityConfig.java
+++ src/main/java/com/porado/backend/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.porado.backend.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,13 +22,13 @@ public class SecurityConfig {
         httpSecurity
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults()) // For postman
                 .formLogin(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 ).authorizeHttpRequests(auth -> auth
-                        //.requestMatchers("/auth/**").permitAll()
-                        .anyRequest().permitAll() // Replace with .fullyAuthenticated() for prod
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().fullyAuthenticated()
                 );
 
         return httpSecurity.build();
