743b9d337ac00b817c88cae794e44fd32f37e169
Gabriel James Benedict <benok.loslos@gmail.com>
2026-02-09 13:22:05 +0800

    feat: add implementations for UserDetails and UserDetailsService

---
+++ src/main/java/com/porado/backend/auth/CustomUserDetails.java
@@ -0,0 +1,32 @@
+package com.porado.backend.auth;
+
+import com.porado.backend.model.User;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole()));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
