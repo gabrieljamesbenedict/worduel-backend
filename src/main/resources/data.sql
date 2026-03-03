fa17a8923257f6a010d8b4bc7e5e3cc7cd6a7509
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-04 01:46:32 +0800

    fix: remove misplaced comma

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (username, password, role)
-VALUES('porado', '$2a$12$1jmum1CAo5rDFfmpgoYeFuz3hFW4ZUAmivmMYwM/yY7fJ0tI1yGRi', 'ROLE_PLAYER'),;
+VALUES('porado', '$2a$12$1jmum1CAo5rDFfmpgoYeFuz3hFW4ZUAmivmMYwM/yY7fJ0tI1yGRi', 'ROLE_PLAYER');
