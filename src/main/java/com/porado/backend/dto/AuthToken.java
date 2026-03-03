0e72cb9303ac79b933c93ecf0edf3c7a81a55cb0
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-03 17:07:49 +0800

    feat: add DTOs for authentication workflows

---
+++ src/main/java/com/porado/backend/dto/AuthToken.java
@@ -0,0 +1,11 @@
+package com.porado.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class AuthToken {
+    private String token;
+}
---
+++ src/main/java/com/porado/backend/dto/UserInfo.java
@@ -0,0 +1,7 @@
+package com.porado.backend.dto;
+
+public class UserInfo {
+    private Long userId;
+    private String username;
+    private String role;
+}
