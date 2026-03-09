38d5560adc8701ff42fe35acc5189787ff57f486
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-10 07:25:11 +0800

    feat: complete GameServiceImpl

--- src/main/java/com/porado/backend/serviceImpl/GameServiceImpl.java
+++ src/main/java/com/porado/backend/serviceImpl/GameServiceImpl.java
@@ -5,6 +5,7 @@ import com.porado.backend.service.GameService;
 import com.porado.backend.service.UserService;
 import com.porado.core.game.GameRoom;
 import com.porado.core.manager.GameRoomManager;
+import com.porado.core.util.GameRoomStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -40,11 +41,16 @@ public class GameServiceImpl implements GameService {
 
     @Override
     public void leaveRoom(Long roomId, Long playerId) {
-
+        User user = userService.getUserById(playerId);
+        GameRoom room = gameRoomManager.getRoom(roomId);
+        if (room.getPlayerList().contains(user)) {
+            room.leavePlayer(user);
+        }
     }
 
     @Override
     public void startGame(Long roomId) {
-
+        GameRoom room = gameRoomManager.getRoom(roomId);
+        room.setStatus(GameRoomStatus.STARTED);
     }
 }
