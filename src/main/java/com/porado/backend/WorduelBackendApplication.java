6510ce4a1d9a15bfe1693f222ab7ef81278a0f66
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-10 11:58:06 +0800

    fix: allow scanning in other directories

--- src/main/java/com/porado/backend/WorduelBackendApplication.java
+++ src/main/java/com/porado/backend/WorduelBackendApplication.java
@@ -3,7 +3,7 @@ package com.porado.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.porado.backend", "com.porado.core"})
 public class WorduelBackendApplication {
 
 	public static void main(String[] args) {
