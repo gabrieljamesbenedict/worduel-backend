e276d45466633887aa7f0fcd9423cef38cd75225
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-11 04:46:33 +0800

    refactor: remove most logic from the game for overhaul

--- src/main/java/com/porado/backend/controller/GameController.java
+++ src/main/java/com/porado/backend/controller/GameController.java
@@ -1,9 +1,5 @@
 package com.porado.backend.controller;
 
-import com.porado.backend.model.User;
-import com.porado.backend.service.GameService;
-import com.porado.backend.service.UserService;
-import com.porado.core.game.GameRoom;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,43 +10,5 @@ import java.util.List;
 @RequiredArgsConstructor
 public class GameController {
 
-    private final GameService gameService;
-    private final UserService userService;
 
-    @PostMapping("/create/{hostId}")
-    public GameRoom createRoom(@PathVariable Long hostId) {
-        User host = userService.getUserById(hostId);
-        return gameService.createRoom(host);
-    }
-
-    @GetMapping("/rooms")
-    public List<GameRoom> searchRooms() {
-        return gameService.searchRooms();
-    }
-
-    @GetMapping("/rooms/{roomId}")
-    public GameRoom getRoom(@PathVariable Long roomId) {
-        return gameService.getRoom(roomId);
-    }
-
-    @PostMapping("/rooms/{roomId}/join/{playerId}")
-    public void joinRoom(
-            @PathVariable Long roomId,
-            @PathVariable Long playerId
-    ) {
-        gameService.joinRoom(roomId, playerId);
-    }
-
-    @PostMapping("/rooms/{roomId}/leave/{playerId}")
-    public void leaveRoom(
-            @PathVariable Long roomId,
-            @PathVariable Long playerId
-    ) {
-        gameService.leaveRoom(roomId, playerId);
-    }
-
-    @PostMapping("/rooms/{roomId}/start")
-    public void startGame(@PathVariable Long roomId) {
-        gameService.startGame(roomId);
-    }
 }
